b7a5e4b3ecd389beba41ee87c602f3a6feae3a28
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-08-16 21:19:22 +0500

    p2-grabbing the active job postin in our view

--- P2-Jobs-Board/job_board/views.py
+++ P2-Jobs-Board/job_board/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import JobPosting
 
 # Create your views here.
 
 
 def index(request):
+    # jobs = JobPosting.objects.all()
+    jobs = JobPosting.objects.filter(is_active=True)
+    print(jobs)
     return HttpResponse("Job Board")
