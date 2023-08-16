661e890e85cc683cc3707651a8cdb63f22913ece
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-08-16 21:25:30 +0500

    p2-listing out active job posting in our view

--- P2-Jobs-Board/job_board/views.py
+++ P2-Jobs-Board/job_board/views.py
@@ -6,7 +6,8 @@ from .models import JobPosting
 
 
 def index(request):
-    # jobs = JobPosting.objects.all()
-    jobs = JobPosting.objects.filter(is_active=True)
-    print(jobs)
-    return HttpResponse("Job Board")
+    active_postings = JobPosting.objects.filter(is_active=True)
+    context = {
+        "job_postings": active_postings,
+    }
+    return render(request, "job_board/index.html", context)
