605721b261b4f14036d8f78707628413cf77746a
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-08-17 21:48:59 +0500

    p3-adding styling to our form with crispy forms

--- P3-Bitly-Clone-Project/config/settings.py
+++ P3-Bitly-Clone-Project/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd Party
+    "crispy_forms",
+    "crispy_tailwind",
     # Local Apps
     "links",
 ]
@@ -123,3 +126,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+
+CRISPY_TEMPLATE_PACK = "tailwind"
