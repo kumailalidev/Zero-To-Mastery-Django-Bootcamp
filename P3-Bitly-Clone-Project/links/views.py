ce2290c24a16659b1f960bc6ecac646b00ee8c10
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-08-17 21:07:32 +0500

    p3-adding link shortening redirect functionality

--- P3-Bitly-Clone-Project/links/views.py
+++ P3-Bitly-Clone-Project/links/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Link
 
 
 def index(request):
     links = Link.objects.all()
-    context = {"links": links}
+    context = {
+        "links": links,
+    }
 
     return render(request, "links/index.html", context)
+
+
+def root_link(request, link_slug):
+    link = get_object_or_404(Link, slug=link_slug)
+    link.click()  # increments click
+    return redirect(link.url)
