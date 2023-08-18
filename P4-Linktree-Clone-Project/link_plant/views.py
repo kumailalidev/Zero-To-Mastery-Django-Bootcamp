dac371e8fb349a876b33feb5fa965fa6a4ff5e96
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-08-19 00:21:22 +0500

    p4-listing out the links with a class based view ListView

--- P4-Linktree-Clone-Project/link_plant/views.py
+++ P4-Linktree-Clone-Project/link_plant/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Profile, Link
 
-# Create your views here.
+
+class LinkListView(ListView):
+    model = Link
