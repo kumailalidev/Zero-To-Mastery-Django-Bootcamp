d74d3f2a397feb89fc505536dcf31f1b33eeaff7
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-08-19 02:43:18 +0500

    p4-updating the links with the UpdateView class

--- P4-Linktree-Clone-Project/link_plant/urls.py
+++ P4-Linktree-Clone-Project/link_plant/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LinkListView, LinkCreateView
+from .views import LinkListView, LinkCreateView, LinkUpdateView
 
 urlpatterns = [
     path("", LinkListView.as_view(), name="link-list"),
     path("link/create/", LinkCreateView.as_view(), name="link-create"),
+    path("link/<int:pk>/update/", LinkUpdateView.as_view(), name="link-update"),
 ]
--- P4-Linktree-Clone-Project/link_plant/views.py
+++ P4-Linktree-Clone-Project/link_plant/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from .models import Profile, Link
 
 
@@ -11,4 +11,10 @@ class LinkListView(ListView):
 class LinkCreateView(CreateView):
     model = Link
     fields = "__all__"
+    success_url = reverse_lazy("link-list")
+
+
+class LinkUpdateView(UpdateView):
+    model = Link
+    fields = ["text", "url"]
     success_url = reverse_lazy("link-list")
