d2d308b67ba55b5f2f53e021b8a6b92e2f0b047f
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-08-19 03:06:19 +0500

    p4-creating the delete view to remove a  link using DeleteView

--- P4-Linktree-Clone-Project/link_plant/urls.py
+++ P4-Linktree-Clone-Project/link_plant/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LinkListView, LinkCreateView, LinkUpdateView
+from .views import LinkDeleteView, LinkListView, LinkCreateView, LinkUpdateView
 
 urlpatterns = [
     path("", LinkListView.as_view(), name="link-list"),
     path("link/create/", LinkCreateView.as_view(), name="link-create"),
     path("link/<int:pk>/update/", LinkUpdateView.as_view(), name="link-update"),
+    path("link/<int:pk>/delete/", LinkDeleteView.as_view(), name="link-delete"),
 ]
--- P4-Linktree-Clone-Project/link_plant/views.py
+++ P4-Linktree-Clone-Project/link_plant/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Profile, Link
 
 
@@ -17,4 +17,9 @@ class LinkCreateView(CreateView):
 class LinkUpdateView(UpdateView):
     model = Link
     fields = ["text", "url"]
+    success_url = reverse_lazy("link-list")
+
+
+class LinkDeleteView(DeleteView):
+    model = Link
     success_url = reverse_lazy("link-list")
